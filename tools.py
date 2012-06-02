8e6b86b4a35536fe604b0fe9cfe7375f714e612c
Родненко Виталий <Skaizer@MacBook-Rodnenko-Vitalij.local>
2012-06-02 18:35:02 +0400

    Доделано редактирование метки. Добавлена проверка изменялась ли метка или нет ранее, чтобы просто так не гонять трафик. Для проверки используется MD5.
    В файл tools.py добавлена функция confirm, котоарая позволяет задать вопрос пользователю, чтобы он ответил Y или N. В ней так же можно указывать ответ по умолчанию, который подсветится в скобочках при выводе.

--- tools.py
+++ tools.py
@@ -15,3 +15,23 @@ def getch():
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
+
+def confirm(prompt_str="Confirm", allow_empty=False, default=False):
+    """
+    Allows to ask user action from console. It asks a question and gives ability to answer Y or N.
+    """
+    fmt = (prompt_str, 'y', 'n') if default else (prompt_str, 'n', 'y')
+    if allow_empty:
+        prompt = '%s [%s]|%s: ' % fmt
+    else:
+        prompt = '%s %s|%s: ' % fmt
+    while True:
+        ans = raw_input(prompt).lower()
+        if ans == '' and allow_empty:
+            return default
+        elif ans == 'y':
+            return True
+        elif ans == 'n':
+            return False
+        else:
+            print 'Please enter y or n.'
