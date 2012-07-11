fe1213e5082b5b7f1c26257b02a1f674ab17871a
d1ffuz0r <d1ffmac@gmail.com>
2012-07-11 13:00:06 +0400

    install without tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='',
     description='Geeknote - is a command line client for Evernote, that can be use on Linux, FreeBSD and OS X.',
     url='http://www.geeknote.me',
-    packages=['unit', 'lib', 'lib.evernote', 'lib.evernote.edam', 'lib.evernote.edam.error', 'lib.evernote.edam.limits',
+    packages=['lib', 'lib.evernote', 'lib.evernote.edam', 'lib.evernote.edam.error', 'lib.evernote.edam.limits',
               'lib.evernote.edam.notestore', 'lib.evernote.edam.type', 'lib.evernote.edam.userstore', 'geeknote'],
     install_requires=[
         'html2text',
