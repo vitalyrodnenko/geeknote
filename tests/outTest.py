90284dd3c8bfc327084f0c4060bd27f0d55347f3
d1ffuz0r <d1fffuz0r@gmail.com>
2012-07-24 21:35:32 +0400

    added test

--- tests/outTest.py
+++ tests/outTest.py
@@ -7,6 +7,7 @@ from geeknote.config import VERSION
 from geeknote.out import printDate, printLine, printAbout,\
     separator, failureMessage, successMessage, showUser, showNote, \
     printList, SearchResult
+from geeknote import out
 
 
 class AccountingStub(object):
@@ -38,12 +39,12 @@ class outTestsWithHackedStdout(unittest.TestCase):
     def test_print_line(self):
         printLine('test')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), 'test\n')
+        self.assertEquals(sys.stdout.read(), 'test\n')
 
     def test_print_line_other_endline_success(self):
         printLine('test', endLine='\n\r')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), 'test\n\r')
+        self.assertEquals(sys.stdout.read(), 'test\n\r')
 
     def test_print_about_success(self):
         about = '''Version: %s
@@ -52,34 +53,34 @@ Use geeknote --help to read documentation.
 And visit www.geeknote.me to check for updates.\n''' % VERSION
         printAbout()
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), about)
+        self.assertEquals(sys.stdout.read(), about)
 
     def test_separator_with_title_success(self):
         line = '------------------- test ------------------\n'
         separator(symbol='-', title='test')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), line)
+        self.assertEquals(sys.stdout.read(), line)
 
     def test_separator_without_title_success(self):
         line = '----------------------------------------\n\n'
         separator(symbol='-')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), line)
+        self.assertEquals(sys.stdout.read(), line)
 
     def test_separator_empty_args_success(self):
         separator()
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), '\n\n')
+        self.assertEquals(sys.stdout.read(), '\n\n')
 
     def test_failure_message_success(self):
         failureMessage('fail')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), 'fail\n')
+        self.assertEquals(sys.stdout.read(), 'fail\n')
 
     def test_success_message_success(self):
         successMessage('success')
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), 'success\n')
+        self.assertEquals(sys.stdout.read(), 'success\n')
 
     def test_show_user_without_fullinfo_success(self):
         showUser(UserStub(), {})
@@ -88,7 +89,7 @@ Username         : testusername
 Name             : testname
 Email            : testemail\n'''
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), info)
+        self.assertEquals(sys.stdout.read(), info)
 
     def test_show_user_with_fullinfo_success(self):
         showUser(UserStub(), True)
@@ -99,7 +100,7 @@ Email            : testemail
 Upload limit     : 0.00
 Upload limit end : 01.01.1970\n'''
         sys.stdout.seek(0)
-        self.assertEqual(sys.stdout.read(), info)
+        self.assertEquals(sys.stdout.read(), info)
 
     def test_show_note_success(self):
         note = '''################## TITLE ##################
@@ -141,10 +142,15 @@ Total found: 2
         self.assertEquals(sys.stdout.read(), notes_list)
 
     def test_print_list_with_selector_success(self):
-        pass  # todo
-
-    def test_print_list_with_selector_success(self):
-        pass  # todo
+        out.rawInput = lambda x: 2
+        notes_list = '''=================== test ==================
+Total found: 2
+  1 : 01.01.1970  testnote
+  2 : 01.01.1970  testnote
+  0 : -Cancel-\n'''
+        out.printList([NoteStub() for _ in xrange(2)], title='test', showSelector=True)
+        sys.stdout.seek(0)
+        self.assertEquals(sys.stdout.read(), notes_list)
 
     def test_search_result_success(self):
         result = '''Search request: test
@@ -156,4 +162,4 @@ Total found: 2
         self.assertEquals(sys.stdout.read(), result)
 
     def test_print_date(self):
-        self.assertEqual(printDate(1000000), '01.01.1970')
+        self.assertEquals(printDate(1000000), '01.01.1970')
